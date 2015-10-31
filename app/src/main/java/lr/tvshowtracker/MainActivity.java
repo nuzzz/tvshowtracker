71a160eb8a3c04d768a212d0affba423bf771977
Linus Teo <fuzznuzz@gmail.com>
2015-10-31 20:47:07 +0800

    Added register and login activity screens without implementation

--- app/src/main/java/lr/tvshowtracker/MainActivity.java
+++ app/src/main/java/lr/tvshowtracker/MainActivity.java
@@ -1,13 +1,11 @@
 package lr.tvshowtracker;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,38 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        initialize_UI();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void initialize_UI(){
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        TextView user = (TextView) findViewById(R.id.userInput);
+        TextView pass = (TextView) findViewById(R.id.passInput);
+
+        TextView registerText =  (TextView) findViewById(R.id.registerText);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void changeToRegisterPage(View v){
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        Intent myIntent = null;
+        myIntent = new Intent(v.getContext() ,RegisterActivity.class);
+        startActivity(myIntent);
     }
 }
---
+++ app/src/main/java/lr/tvshowtracker/RegisterActivity.java
@@ -0,0 +1,52 @@
+package lr.tvshowtracker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by linus on 22/10/2015.
+ */
+public class RegisterActivity extends AppCompatActivity{
+
+    TextView errorText;
+    EditText username;
+    EditText password;
+    EditText password2;
+    String user, pass1, pass2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_user);
+
+        //initialise UI
+        initializeUI();
+
+        //TODO: not finished yet
+        //username.addTextChangedListener(new TextWatcher());
+        //password.addTextChangedListener(new TextWatcher());
+        //password2.addTextChangedListener(new TextWatcher());
+    }
+
+    private void initializeUI() {
+        errorText = (EditText) findViewById(R.id.username);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password1);
+        password2 = (EditText) findViewById(R.id.password2);
+    }
+
+
+    private void validateUser(EditText username, String user) {
+
+    }
+
+    private void validatePass(String pass, String pass2) {
+    }
+
+
+
+}
